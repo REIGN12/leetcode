e1f0849d5b875ba2a898cd54d88446f7ed07ca38
REIGN12 <3180105323@zju.edu.cn>
2021-02-09 00:09:02 +0800

    p3: Adding num, sig, blank support

--- lengthOfLongestSubstring.c
+++ lengthOfLongestSubstring.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
-#define NUM 26
+// 输入包括英文字母，数字，符号，空格
+#define NUM (126-32+1)
 int lengthOfLongestSubstring(char* s)
 {
+    if(!*s) return 0;
     // albet for booking idx
     int albet[NUM]; for(int i = 0;i<NUM;++i)albet[i] = -1;
     int i=0,j=0;
-    albet[s[0]-'a'] = 0;
+    albet[s[0]-' '] = 0;
     int res = 0;
     while (1)
     {
         // move j
-        while (s[++j] && albet[s[j]-'a']<0) albet[s[j]-'a'] = j;
+        while (s[++j] && albet[s[j]-' ']<0) albet[s[j]-' '] = j;
         // update res
         if(j-i > res) res = j-i;
         // break condition
         if(!s[j]) break;
         // move i
-        i = albet[s[j]-'a'] + 1;
+        i = albet[s[j]-' '] + 1;
         // update albet
         for(int l = 0;l<NUM;++l)
         {
             if (albet[l] < i) albet[l] = -1;
         }
-        albet[s[i]-'a'] = i;    
-        albet[s[j]-'a'] = j;    
+        albet[s[i]-' '] = i;    
+        albet[s[j]-' '] = j;    
     }
     return res;
 }
